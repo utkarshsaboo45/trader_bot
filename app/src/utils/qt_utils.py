af5f6f3e72415cc29c62429694cfe1220ccaa549
Utkarsh Saboo <usaboo@globeop.com>
2026-01-04 00:01:53 -0800

    Add Headers

--- app/src/utils/qt_utils.py
+++ app/src/utils/qt_utils.py
@@ -125,7 +125,12 @@ def get_access_token(refresh_token=None):
             refresh_token = file.read()
 
     url = f'{URL_ACCESS_TOKEN}{refresh_token}'
-    response = requests.get(url)
+
+    headers_access = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    response = requests.get(url, headers=headers_access, timeout=20)
     response.raise_for_status()
     dict_token_data = response.json()
 
