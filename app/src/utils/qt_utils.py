77e7c6ba46e654c6b7052d8b79dff75f6b83b34a
Utkarsh Saboo <usaboo@globeop.com>
2026-01-04 00:25:31 -0800

    Disable Headers

--- app/src/utils/qt_utils.py
+++ app/src/utils/qt_utils.py
@@ -130,8 +130,14 @@ def get_access_token(refresh_token=None):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
 
-    response = requests.get(url, headers=headers_access, timeout=20)
+    response = requests.get(
+        url,
+        # headers=headers_access,
+        timeout=20,
+    )
+
     response.raise_for_status()
+
     dict_token_data = response.json()
 
     access_token = dict_token_data['access_token']
